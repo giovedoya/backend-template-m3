ce25d45aa67db952a716f0ad00fb1378939cbb57
Giovanni Bedoya <giovedoya@gmail.com>
2023-03-31 21:50:07 +0200

    BlogPost model

---
+++ models/BlogPost.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const blogPostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    default:
+      "https://www.genius100visions.com/wp-content/uploads/2017/09/placeholder-vertical.jpg",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = model("BlogPost", blogPostSchema);
