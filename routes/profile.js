992fc730591177e568a488f980e0a5aab282f197
Giovanni Bedoya <giovedoya@gmail.com>
2023-04-07 17:06:09 +0200

    update routes profile in progress

--- routes/profile.js
+++ routes/profile.js
@@ -6,43 +6,49 @@ const { isAuthenticated } = require('../middlewares/jwt');
 // @route   GET /profile
 // @access  Private
 router.get('/', isAuthenticated, async (req, res, next) => {
-  const userId = req.payload._id;
   try {
-    const userDB = await User.findById(userId);
-    res.status(200).json({
-      user: {
-        username: userDB.username,
-        email: userDB.email,
-      }
-    });
+    const userDB = await User.find();
+    res.status(200).json(userDB);
   } catch (error) {
     next(error);
   }
 });
 
+// @desc    Get one user
+// @route   GET /user/:userId
+// @access  Public
+router.get("/:userId", async (req, res, next) => {
+    const { userId } = req.params;
+    try {
+      const user = await User.findById(userId);
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+
+
+
 // @desc    Edit profile
 // @route   PUT /profile
 // @access  Private
 router.put('/', isAuthenticated, async (req, res, next) => {
-  const userId = req.payload._id;
-  const { username, email, password } = req.body;
-  try {
-    const userDB = await User.findById(userId);
-    userDB.username = username || userDB.username;
-    userDB.email = email || userDB.email;
-    if (password) {
-      userDB.password = password;
+    const { _id: userId } = req.payload;
+    const { username, email } = req.body;
+  
+    if (!username || !email) {
+      return res.status(400).json({ error: "Please fill all the fields in order to update your profile." });
     }
-    await userDB.save();
-    res.status(200).json({
-      user: {
-        username: userDB.username,
-        email: userDB.email,
-      }
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+  
+    const allowedFields = { username, email }; 
+    try {
+      const updatedUser = await User.findByIdAndUpdate(userId, allowedFields, { new: true });
+      return res.status(200).json({ user: { username: updatedUser.username, email: updatedUser.email } });
+    } catch (error) {
+      return next(error);
+    }
+  });
+  
 
 module.exports = router;
