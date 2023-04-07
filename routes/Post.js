df98414372bdce77a01633bcc59d3e051d2f3d1b
Giovanni Bedoya <giovedoya@gmail.com>
2023-04-07 06:11:33 +0200

    Post and Dress error solution

--- routes/Post.js
+++ routes/Post.js
@@ -1,14 +1,15 @@
 const router = require("express").Router();
-const BlogPost = require('../models/BlogPost');
+const BlogPost = require("../models/BlogPost");
 const { isAdmin } = require("../middlewares/jwt");
+const fileUploader = require("../config/cloudinary.config");
 
-const { isAuthenticated } = require('../middlewares/jwt');
+const { isAuthenticated } = require("../middlewares/jwt");
 // @desc    Get all post
 // @route   GET /post
 // @access  Public
 router.get("/", async (req, res, next) => {
   try {
-    const posts = await BlogPost.find()
+    const posts = await BlogPost.find();
     res.status(200).json(posts);
   } catch (error) {
     next(error);
@@ -21,49 +22,68 @@ router.get("/", async (req, res, next) => {
 router.get("/:postId", async (req, res, next) => {
   const { postId } = req.params;
   try {
-    const post = await BlogPost.findById(postId)
+    const post = await BlogPost.findById(postId);
     res.status(200).json(post);
   } catch (error) {
     next(error);
   }
 });
 
+// @desc    Upload image
+// @route   POST /upload
+// @access  Private
+router.post(
+  "/upload",
+  isAuthenticated,
+  fileUploader.single("imageUrl"),
+  (req, res, next) => {
+    if (!req.file) {
+      next(new Error("No file uploaded!"));
+      return;
+    }
+    res.json({ fileUrl: req.file.path });
+  }
+);
+
 // @desc    Create post
 // @route   POST /post
 // @access  Private
-router.post('/', isAuthenticated, isAdmin, async (req, res, next) => {
-    const { title, content, author, image } = req.body
-    if (!title || !content || !author || !image ) {
-        return res.status(400).json({ message: "please fill in all the fields" });
-      }
-        try {
-          const newPost = await BlogPost.create({ title, content, author, image });
-          res.status(201).json(newPost);
-        } catch (error) {
-          next(error)
-        }
-      });
+router.post("/", isAuthenticated, isAdmin, async (req, res, next) => {
+  const { title, content, author, image } = req.body;
+  if (!title || !content || !author) {
+    return res.status(400).json({ message: "please fill in all the fields" });
+  }
+  try {
+    const newPost = await BlogPost.create({ title, content, author, image });
+    res.status(201).json(newPost);
+  } catch (error) {
+    next(error);
+  }
+});
 
-// @desc    Edit a post 
+// @desc    Edit a post
 // @route   PUT /post/:posId
 // @access  Private
-router.put("/:postId",  async (req, res, next) => {
-    const { postId } = req.params;
-    const { title, content, author, image } = req.body
-    if (!title || !content || !author || !image ) {
-        return res.status(400).json({ message: "please fill in all the fields" });
-      }
-    try {
-      const updatedPost = await BlogPost.findByIdAndUpdate(postId, { title, content, author, image }, { new: true })
-      if (!updatedPost) {
-        return res.status(404).json({ message: "Post not found" });
-      };
-      res.status(200).json(updatedPost);
-    } catch (error) {
-      next(error);
-    }
+router.put("/:postId", async (req, res, next) => {
+  const { postId } = req.params;
+  const { title, content, author, image } = req.body;
+  if (!title || !content || !author || !image) {
+    return res.status(400).json({ message: "please fill in all the fields" });
   }
-);
+  try {
+    const updatedPost = await BlogPost.findByIdAndUpdate(
+      postId,
+      { title, content, author, image },
+      { new: true }
+    );
+    if (!updatedPost) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // @desc    Delete a post
 // @route   DELETE /post/:posId
@@ -71,8 +91,8 @@ router.put("/:postId",  async (req, res, next) => {
 router.delete("/:postId", async (req, res, next) => {
   const { postId } = req.params;
   try {
-     await BlogPost.findById(postId);
-    await BlogPost.findByIdAndDelete(postId);   
+    await BlogPost.findById(postId);
+    await BlogPost.findByIdAndDelete(postId);
     res.status(204).json({ message: "the post has been removed successfully" });
   } catch (error) {
     next(error);
--- routes/dress.js
+++ routes/dress.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const Dress = require("../models/Dress");
-const { isAuthenticated } = require('../middlewares/jwt');
+const { isAuthenticated } = require("../middlewares/jwt");
 const validateDress = require("../utils");
 const fileUploader = require("../config/cloudinary.config");
-
 
 // @desc    Get all dress
 // @route   GET /dress
@@ -30,62 +29,83 @@ router.get("/:dressId", async (req, res, next) => {
   }
 });
 
-
 // @desc    Get search dress
 // @route   GET /dress/search
 // @access  Public
 router.get("/search", async (req, res, next) => {
-  console.log('ruta encontrada')
+  console.log("ruta encontrada");
   try {
     const { neckline, court, size, long } = req.query;
-    const dresses = await Dress.find({ neckline, court, size, long }).populate("seller");
+    const dresses = await Dress.find({ neckline, court, size, long }).populate(
+      "seller"
+    );
     res.status(200).json(dresses);
   } catch (error) {
     next(error);
-    console.error(error)
+    console.error(error);
   }
 });
-
-
-
 
 // @desc    Upload image
 // @route   POST /upload
 // @access  Private
-router.post("/upload", isAuthenticated, fileUploader.single("imageUrl"), (req, res, next) => {
-  if (!req.file) {
-    next(new Error("No file uploaded!"));
-    return;
+router.post(
+  "/upload",
+  isAuthenticated,
+  fileUploader.single("imageUrl"),
+  (req, res, next) => {
+    if (!req.file) {
+      next(new Error("No file uploaded!"));
+      return;
+    }
+    res.json({ fileUrl: req.file.path });
   }
-  res.json({ fileUrl: req.file.path });
-});
-
+);
 
 // @desc    Create dress
 // @route   POST /dress
 // @access  Private
 router.post("/", isAuthenticated, async (req, res, next) => {
-  const { neckline, court, long, color, size, designer, name, description, price, location, image } = req.body;
+  const {
+    neckline,
+    court,
+    long,
+    color,
+    size,
+    designer,
+    name,
+    description,
+    price,
+    location,
+    image,
+  } = req.body;
   const dressIsValid = validateDress(req.body);
   console.log(dressIsValid);
-  if (dressIsValid.isValid === false){
-    res.status(400).json({message: "Please check your fields"});
-  } else{
+  if (dressIsValid.isValid === false) {
+    res.status(400).json({ message: "Please check your fields" });
+  } else {
     const seller = req.payload._id;
-      try {
-        const newDress = await Dress.create({
-          neckline, court, long, color, size, designer, name, description, price, location, image,
-          seller: seller
-        }).populate('seller');
-        res.status(201).json(newDress)
-      } catch (error) {
-        next(error);
-      }
+    try {
+      const newDress = await Dress.create({
+        neckline,
+        court,
+        long,
+        color,
+        size,
+        designer,
+        name,
+        description,
+        price,
+        location,
+        image,
+        seller: seller,
+      }).populate("seller");
+      res.status(201).json(newDress);
+    } catch (error) {
+      next(error);
+    }
   }
 });
-
-
-
 
 // @desc    Delete one dress
 // @route   DELETE /dress/:dressId
@@ -96,42 +116,71 @@ router.delete("/:dressId", isAuthenticated, async (req, res, next) => {
   try {
     const deletedDress = await Dress.findById(dressId);
     if (deletedDress.seller.toString() !== seller) {
-      return res.status(403).json({ message: "You are not allowed to delete this dress" });
+      return res
+        .status(403)
+        .json({ message: "You are not allowed to delete this dress" });
     }
     await Dress.findByIdAndDelete(dressId);
-    res.status(204).json({ message: "the dress has been removed successfully" });
+    res
+      .status(204)
+      .json({ message: "the dress has been removed successfully" });
   } catch (error) {
     next(error);
   }
 });
 
-
 // @desc    Edit one dress
 // @route   PUT /dress/:dressId
 // @access  Private
+router.put("/:dressId", isAuthenticated, async (req, res, next) => {
+  const { dressId } = req.params;
+  const seller = req.payload._id;
+  const {
+    neckline,
+    court,
+    long,
+    color,
+    size,
+    designer,
+    name,
+    description,
+    price,
+    location,
+    image,
+  } = req.body;
 
-  router.put("/:dressId", isAuthenticated, async (req, res, next) => {
-    const { dressId } = req.params;
-
-    const seller = req.payload._id;
-
-    const { neckline, court, long, color, size, designer, name, description, price, location, image } = req.body;
-    
-    const validation = validateDress(req.body);
-    if (!validation.isValid) {
-      res.status(400).json({ message: "Please check your fields", missingFields: validation.missingFields });
-    } else {
+  const validation = validateDress(req.body);
+  if (!validation.isValid) {
+    res
+      .status(400)
+      .json({
+        message: "Please check your fields",
+        missingFields: validation.missingFields,
+      });
+  } else {
     try {
       const editedDress = await Dress.findOneAndUpdate(
         { _id: dressId, seller },
-        { neckline, court, long, color, size, designer, name, description, price, location, image },
+        {
+          neckline,
+          court,
+          long,
+          color,
+          size,
+          designer,
+          name,
+          description,
+          price,
+          location,
+          image,
+        },
         { new: true }
-      ).populate('seller');
+      ).populate("seller");
 
       if (!editedDress) {
         return res.status(404).json({ message: "Dress not found" });
       }
-      res.status(200).json(editedDress)
+      res.status(200).json(editedDress);
     } catch (error) {
       console.error("Error in updating dress:", error);
       res.status(500).json({ message: "Internal server error" });
